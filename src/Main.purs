b7d7ad034a92358469eb49eb1ada8217af2a13e5
Ryan DV <rdevilla@uwaterloo.ca>
2017-12-19 00:46:08 -0500

    Print Spawn energy capacity

--- src/Main.purs
+++ src/Main.purs
@@ -1,14 +1,22 @@
 module Main where
 
 import Prelude
+
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 
+import Data.Maybe
+import Data.StrMap as M
+
 import Screeps
 import Screeps.Game as Game
+import Screeps.Spawn as Spawn
 
 main :: forall e. Eff (cmd :: CMD, console :: CONSOLE, tick :: TICK, time :: TIME | e) Unit
 main = do
   game <- Game.getGameGlobal
-  startUsed <- Game.getUsed game
-  log $ "Start: " <> show startUsed
+  let spawn = M.lookup "Spawn1" $ Game.spawns game
+  maybe
+    (log "No spawn detected")
+    (\spawn -> log $ "Spawn " <> show (Spawn.name spawn) <> "[" <> show (Spawn.energy spawn) <> "]")
+    spawn
