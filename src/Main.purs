571ed65a6ef698ad6129b6759f5ba8705e100d17
Ryan DV <rdevilla@uwaterloo.ca>
2017-12-19 02:01:54 -0500

    Extract functions and add scaffold for Creep actions

--- src/Main.purs
+++ src/Main.purs
@@ -8,6 +8,7 @@ import Control.Monad.Eff.Console (CONSOLE, log)
 import Data.Either
 import Data.Maybe
 import Data.StrMap as M
+import Data.Traversable
 
 import Screeps
 import Screeps.Constants
@@ -17,12 +18,27 @@ import Screeps.Spawn as Spawn
 main :: forall e. Eff (cmd :: CMD, console :: CONSOLE, tick :: TICK, time :: TIME | e) Unit
 main = do
   game <- Game.getGameGlobal
+  let creeps = Game.creeps game
   let spawn = M.lookup "Spawn1" $ Game.spawns game
   maybe (log "No spawn detected") doSpawnActions spawn
+  traverse doCreepActions creeps
+  pure unit
+
+doCreepActions :: Creep -> forall e. Eff (cmd :: CMD, console :: CONSOLE, tick :: TICK, time :: TIME | e) Unit
+doCreepActions creep = pure unit
 
 doSpawnActions :: Spawn -> forall e. Eff (cmd :: CMD, console :: CONSOLE, tick :: TICK, time :: TIME | e) Unit
 doSpawnActions spawn = do
-  when (Spawn.canCreateCreep spawn [part_move, part_work, part_carry] == ok) $ do
-    createCreepResult <- Spawn.createCreep spawn [part_move, part_work, part_carry]
-    either (\returnCode -> (log $ "Failed to create creep: " <> show returnCode)) (const $ pure unit) createCreepResult
-  log $ "Spawn " <> show (Spawn.name spawn) <> ": " <> show (Spawn.energy spawn)
+  when (Spawn.canCreateCreep spawn energyParts == ok) $ do
+    createCreepResult <- Spawn.createCreep spawn energyParts
+    either doLogReturnCode (const $ pure unit) createCreepResult
+  doLogSpawnEnergy spawn
+
+doLogReturnCode :: ReturnCode -> forall e. Eff (cmd :: CMD, console :: CONSOLE, tick :: TICK, time :: TIME | e) Unit
+doLogReturnCode returnCode = log $ "Failed to create creep: " <> show returnCode
+
+doLogSpawnEnergy :: Spawn -> forall e. Eff (cmd :: CMD, console :: CONSOLE, tick :: TICK, time :: TIME | e) Unit
+doLogSpawnEnergy spawn = log $ "Spawn " <> show (Spawn.name spawn) <> ": " <> show (Spawn.energy spawn)
+
+energyParts :: Array BodyPartType
+energyParts = [part_move, part_work, part_carry]
