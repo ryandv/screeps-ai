215a530614b26e22d1f58af069d73d53d662155b
Ryan DV <rdevilla@uwaterloo.ca>
2017-12-19 01:33:25 -0500

    Spawn creeps whenever they can be created

--- src/Main.purs
+++ src/Main.purs
@@ -5,10 +5,12 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 
+import Data.Either
 import Data.Maybe
 import Data.StrMap as M
 
 import Screeps
+import Screeps.Constants
 import Screeps.Game as Game
 import Screeps.Spawn as Spawn
 
@@ -16,7 +18,11 @@ main :: forall e. Eff (cmd :: CMD, console :: CONSOLE, tick :: TICK, time :: TIM
 main = do
   game <- Game.getGameGlobal
   let spawn = M.lookup "Spawn1" $ Game.spawns game
-  maybe
-    (log "No spawn detected")
-    (\spawn -> log $ "Spawn " <> show (Spawn.name spawn) <> "[" <> show (Spawn.energy spawn) <> "]")
-    spawn
+  maybe (log "No spawn detected") doSpawnActions spawn
+
+doSpawnActions :: Spawn -> forall e. Eff (cmd :: CMD, console :: CONSOLE, tick :: TICK, time :: TIME | e) Unit
+doSpawnActions spawn = do
+  when (Spawn.canCreateCreep spawn [part_move, part_work, part_carry] == ok) $ do
+    createCreepResult <- Spawn.createCreep spawn [part_move, part_work, part_carry]
+    either (\returnCode -> (log $ "Failed to create creep: " <> show returnCode)) (const $ pure unit) createCreepResult
+  log $ "Spawn " <> show (Spawn.name spawn) <> ": " <> show (Spawn.energy spawn)
