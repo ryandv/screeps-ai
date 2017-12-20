0b10a3b8fb66547f09d2d1cfdc51b62c041572f2
Ryan DV <rdevilla@uwaterloo.ca>
2017-12-20 02:01:39 -0500

    Store Creeps and the object they are operating on in memory

--- src/Main.purs
+++ src/Main.purs
@@ -5,10 +5,14 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 
+import Data.Argonaut.Core
+import Data.Argonaut.Encode
 import Data.Array
 import Data.Either
 import Data.Maybe
+import Data.Semigroup
 import Data.StrMap as M
+import Data.Tuple
 import Data.Traversable
 
 import Screeps
@@ -16,9 +20,18 @@ import Screeps.Constants
 import Screeps.Creep as Creep
 import Screeps.FFI as FFI
 import Screeps.Game as Game
+import Screeps.Memory as Memory
 import Screeps.Room as Room
 import Screeps.RoomObject as RoomObject
 import Screeps.Spawn as Spawn
+
+newtype Assignment = Assignment { assignee :: Creep, target :: String }
+
+instance encodeJsonAssignment :: EncodeJson Assignment where
+  encodeJson (Assignment ass) = fromObject $ M.fromFoldable
+    [ Tuple "assignee" (fromString <<< Creep.name $ ass.assignee)
+    , Tuple "target" (fromString ass.target)
+    ]
 
 main :: forall e. Eff (cmd :: CMD, console :: CONSOLE, tick :: TICK, time :: TIME, memory :: MEMORY | e) Unit
 main = do
@@ -28,20 +41,23 @@ main = do
   let spawn = M.lookup "Spawn1" $ Game.spawns game
 
   maybe (log "No spawn detected") doSpawnActions spawn
-  traverse doCreepActions creeps
+  assignments <- traverse doCreepActions creeps
+
+  mem <- Memory.getMemoryGlobal
+  Memory.set mem "assignments" (encodeJson assignments)
 
   pure unit
 
-doCreepActions :: Creep -> forall e. Eff (cmd :: CMD, console :: CONSOLE, tick :: TICK, time :: TIME, memory :: MEMORY | e) Unit
+doCreepActions :: Creep -> forall e. Eff (cmd :: CMD, console :: CONSOLE, tick :: TICK, time :: TIME, memory :: MEMORY | e) (Maybe Assignment)
 doCreepActions creep = do
   let room = RoomObject.room creep
   let sources = Room.find room find_sources
 
   let source = head sources
 
-  maybe (log "No more sources") (doCollectEnergy creep) source
+  maybe (log "No more sources" >>= (const $ pure Nothing)) (doCollectEnergy creep) source
 
-doCollectEnergy :: Creep -> Source -> forall e. Eff (cmd :: CMD, console :: CONSOLE, tick :: TICK, time :: TIME, memory :: MEMORY | e) Unit
+doCollectEnergy :: Creep -> Source -> forall e. Eff (cmd :: CMD, console :: CONSOLE, tick :: TICK, time :: TIME, memory :: MEMORY | e) (Maybe Assignment)
 doCollectEnergy creep source = do
   moveToSourceResult <- (Creep.moveTo creep (TargetObj source))
 
@@ -49,6 +65,8 @@ doCollectEnergy creep source = do
   when (moveToSourceResult == ok) $ do
     harvestSourceResult <- Creep.harvestSource creep source
     when (harvestSourceResult /= ok) $ doLogReturnCode "HARVEST_SOURCE" harvestSourceResult
+
+  pure <<< Just $ Assignment { assignee: creep, target: FFI.unsafeField "id" source }
 
 doSpawnActions :: Spawn -> forall e. Eff (cmd :: CMD, console :: CONSOLE, tick :: TICK, time :: TIME | e) Unit
 doSpawnActions spawn = do
