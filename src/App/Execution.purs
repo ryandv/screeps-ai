7594593866c927a4f88a4e17363c2507227eec88
Ryan DV <rdevilla@uwaterloo.ca>
2017-12-26 06:31:29 -0500

    Extract an App.Execution module

---
+++ src/App/Execution.purs
@@ -0,0 +1,91 @@
+module App.Execution where
+
+import Prelude
+
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Class (liftEff)
+import Control.Monad.Eff.Console (log)
+import Control.Monad.Except.Trans (ExceptT, runExceptT, throwError)
+
+import Data.Array (head)
+import Data.Either (Either(..), either)
+import Data.Maybe (Maybe(..), maybe)
+import Data.StrMap as M
+
+import Screeps (BodyPartType, ReturnCode, TargetPosition(..))
+import Screeps.Constants (look_sources, ok, part_carry, part_move, part_work, resource_energy)
+import Screeps.Creep as Creep
+import Screeps.Game as Game
+import Screeps.Room as Room
+import Screeps.RoomObject as RoomObject
+import Screeps.RoomPosition as RoomPosition
+import Screeps.Spawn as Spawn
+
+import Types
+
+executeInstruction :: Instruction -> Eff BaseScreepsEffects (Maybe Observation)
+executeInstruction (DoNothing creepName) = pure Nothing
+executeInstruction SpawnCreep = do
+  game <- Game.getGameGlobal
+
+  let maybeSpawn = M.lookup "Spawn1" $ Game.spawns game
+
+  maybe (pure $ Just CannotSpawnCreep) (\spawn -> do
+        createCreepResult <- Spawn.createCreep spawn energyParts
+        pure $ either (const $ Just CannotSpawnCreep) (const Nothing) createCreepResult) maybeSpawn
+executeInstruction (MoveTo creepName (Point x y)) = do
+  log $ "Executing MoveTo(" <> creepName <> "," <> show x <> "," <> show y <> ")"
+  game <- Game.getGameGlobal
+  observation <- runExceptT do
+    let maybeCreep = M.lookup creepName $ Game.creeps game
+
+    maybe (throwError UndistinguishedErrors) (\creep -> do
+      doTryCommand "MOVE_CREEP" $ Creep.moveTo creep (TargetPt x y)
+      pure $ Nothing) maybeCreep
+
+  pure $ either (const Nothing) id $ observation
+executeInstruction (HarvestSource creepName (Point x y)) = do
+  log $ "Executing HarvestSource(" <> creepName <> "," <> show x <> "," <> show y <> ")"
+  game <- Game.getGameGlobal
+  observation <- runExceptT do
+    let maybeCreep = M.lookup creepName $ Game.creeps game
+
+    maybe (throwError UndistinguishedErrors) (\creep -> do
+      let eitherSource = RoomPosition.lookFor (RoomPosition.mkRoomPosition x y (Room.name (RoomObject.room creep))) look_sources
+
+      case eitherSource of
+           (Right sources) -> maybe (pure $ Nothing) (\source -> do
+             doTryCommand "HARVEST_SOURCE" $ Creep.harvestSource creep source
+             pure Nothing) $ head sources
+           _ -> pure $ Nothing
+
+      ) maybeCreep
+
+  pure $ either (const Nothing) id $ observation
+executeInstruction (TransferEnergyTo creepName (Point x y)) = do
+  log $ "Executing TransferEnergyTo(" <> creepName <> "," <> show x <> "," <> show y <> ")"
+  game <- Game.getGameGlobal
+  observation <- runExceptT do
+    let maybeCreep = M.lookup creepName $ Game.creeps game
+
+    maybe (throwError UndistinguishedErrors) (\creep ->
+      case Room.controller (RoomObject.room creep) of
+        Just controller -> do
+          doTryCommand "TRANSFER_ENERGY_TO_CONTROLLER" $ Creep.transferToStructure creep controller resource_energy
+          pure Nothing
+        _ -> pure $ Nothing) maybeCreep
+
+  pure $ either (const Nothing) id $ observation
+
+energyParts :: Array BodyPartType
+energyParts = [part_move, part_work, part_carry]
+
+doTryCommand :: String -> Eff BaseScreepsEffects ReturnCode -> ExceptT CommandError (Eff BaseScreepsEffects) Unit
+doTryCommand commandName command = do
+ result <- (liftEff command)
+ when (result == ok) $ (pure unit)
+ when (result /= ok) $ case result of
+                            err_not_enough_energy -> throwError OutOfEnergyError
+                            err_not_enough_resources -> throwError OutOfResourcesError
+                            err_not_in_range -> throwError OutOfRangeError
+                            _ -> throwError UndistinguishedErrors
--- src/Main.purs
+++ src/Main.purs
@@ -3,14 +3,12 @@ module Main where
 import Prelude
 
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (log)
-import Control.Monad.Except.Trans (ExceptT, runExceptT, throwError)
 import Control.Monad.State.Trans (StateT(..), runStateT)
 
 import Data.Argonaut.Encode (encodeJson)
 import Data.Array (catMaybes, concat, filter, head, tail, (:))
-import Data.Either (Either(..), either)
+import Data.Either (Either, either)
 import Data.Identity (Identity(..))
 import Data.Int as Int
 import Data.Maybe (Maybe(..), maybe)
@@ -19,8 +17,8 @@ import Data.StrMap as M
 import Data.Tuple (Tuple(..), fst, snd)
 import Data.Traversable (Accum, foldl, mapAccumL, traverse)
 
-import Screeps (BodyPartType, Creep, ReturnCode, TargetPosition(..))
-import Screeps.Constants (find_sources, look_sources, ok, part_carry, part_move, part_work, resource_energy)
+import Screeps (Creep, ReturnCode)
+import Screeps.Constants (find_sources)
 import Screeps.Controller as Controller
 import Screeps.Creep as Creep
 import Screeps.Game as Game
@@ -28,8 +26,8 @@ import Screeps.Memory as Memory
 import Screeps.Room as Room
 import Screeps.RoomObject as RoomObject
 import Screeps.RoomPosition as RoomPosition
-import Screeps.Spawn as Spawn
 
+import App.Execution as Execution
 import Types
 
 mainLoop :: Eff BaseScreepsEffects Unit
@@ -42,7 +40,7 @@ mainLoop = do
 
   instructionQueue <- getInstructionQueue
   let creepInstructionQueue = catMaybes $ map head $ M.fold (\acc key val -> val:acc) [] $ getCreepInstructions state
-  instructionResultObservations <- catMaybes <$> traverse executeInstruction (instructionQueue <> creepInstructionQueue)
+  instructionResultObservations <- catMaybes <$> traverse Execution.executeInstruction (instructionQueue <> creepInstructionQueue)
 
   let instructionsAndNextState = (unwrap $ runStateT (StateT (generateInstructions (reportObservations <> instructionResultObservations))) state) :: Tuple (Array Instruction) AiState
 
@@ -135,59 +133,6 @@ getInstructionQueue = do
   instructionQueue <- (Memory.get mem "instructionQueue") :: forall e. (EffScreepsCommand e) (Either String (Array Instruction))
   pure $ either (const []) id instructionQueue
 
-executeInstruction :: Instruction -> Eff BaseScreepsEffects (Maybe Observation)
-executeInstruction (DoNothing creepName) = pure Nothing
-executeInstruction SpawnCreep = do
-  game <- Game.getGameGlobal
-
-  let maybeSpawn = M.lookup "Spawn1" $ Game.spawns game
-
-  maybe (pure $ Just CannotSpawnCreep) (\spawn -> do
-        createCreepResult <- Spawn.createCreep spawn energyParts
-        pure $ either (const $ Just CannotSpawnCreep) (const Nothing) createCreepResult) maybeSpawn
-executeInstruction (MoveTo creepName (Point x y)) = do
-  log $ "Executing MoveTo(" <> creepName <> "," <> show x <> "," <> show y <> ")"
-  game <- Game.getGameGlobal
-  observation <- runExceptT do
-    let maybeCreep = M.lookup creepName $ Game.creeps game
-
-    maybe (throwError UndistinguishedErrors) (\creep -> do
-      doTryCommand "MOVE_CREEP" $ Creep.moveTo creep (TargetPt x y)
-      pure $ Nothing) maybeCreep
-
-  pure $ either (const Nothing) id $ observation
-executeInstruction (HarvestSource creepName (Point x y)) = do
-  log $ "Executing HarvestSource(" <> creepName <> "," <> show x <> "," <> show y <> ")"
-  game <- Game.getGameGlobal
-  observation <- runExceptT do
-    let maybeCreep = M.lookup creepName $ Game.creeps game
-
-    maybe (throwError UndistinguishedErrors) (\creep -> do
-      let eitherSource = RoomPosition.lookFor (RoomPosition.mkRoomPosition x y (Room.name (RoomObject.room creep))) look_sources
-
-      case eitherSource of
-           (Right sources) -> maybe (pure $ Nothing) (\source -> do
-             doTryCommand "HARVEST_SOURCE" $ Creep.harvestSource creep source
-             pure Nothing) $ head sources
-           _ -> pure $ Nothing
-
-      ) maybeCreep
-
-  pure $ either (const Nothing) id $ observation
-executeInstruction (TransferEnergyTo creepName (Point x y)) = do
-  log $ "Executing TransferEnergyTo(" <> creepName <> "," <> show x <> "," <> show y <> ")"
-  game <- Game.getGameGlobal
-  observation <- runExceptT do
-    let maybeCreep = M.lookup creepName $ Game.creeps game
-
-    maybe (throwError UndistinguishedErrors) (\creep ->
-      case Room.controller (RoomObject.room creep) of
-        Just controller -> do
-          doTryCommand "TRANSFER_ENERGY_TO_CONTROLLER" $ Creep.transferToStructure creep controller resource_energy
-          pure Nothing
-        _ -> pure $ Nothing) maybeCreep
-
-  pure $ either (const Nothing) id $ observation
 
 getStateFromMemory :: Eff BaseScreepsEffects AiState
 getStateFromMemory = do
@@ -285,22 +230,5 @@ main = do
 
   pure unit
 
-doTryCommand :: String -> Eff BaseScreepsEffects ReturnCode -> ExceptT CommandError (Eff BaseScreepsEffects) Unit
-doTryCommand commandName command = do
- result <- (liftEff command)
- when (result == ok) $ (pure unit)
- when (result /= ok) $ case result of
-                            err_not_enough_energy -> doThrowError commandName result OutOfEnergyError
-                            err_not_enough_resources -> doThrowError commandName result OutOfResourcesError
-                            err_not_in_range -> doThrowError commandName result OutOfRangeError
-                            _ -> doThrowError commandName result UndistinguishedErrors
-
-doThrowError :: String -> ReturnCode -> CommandError -> ExceptT CommandError (Eff BaseScreepsEffects) Unit
-doThrowError commandName result error = do
-  throwError error
-
 doLogReturnCode :: String -> ReturnCode -> forall e. (EffScreepsCommand e) Unit
 doLogReturnCode actionName returnCode = log $ "[FAILED " <> actionName <> "]:" <> show returnCode
-
-energyParts :: Array BodyPartType
-energyParts = [part_move, part_work, part_carry]
