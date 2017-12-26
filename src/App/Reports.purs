4dac8454973f7d64360f5310e3609c54366b3de2
Ryan DV <rdevilla@uwaterloo.ca>
2017-12-26 15:20:57 -0500

    Extract an App.Reports module

---
+++ src/App/Reports.purs
@@ -0,0 +1,55 @@
+module App.Reports(generateReports) where
+
+import Prelude
+
+import Control.Monad.Eff (Eff)
+
+import Data.Either (Either, either)
+import Data.Int as Int
+import Data.Maybe (Maybe(..), maybe)
+import Data.StrMap as M
+import Data.Tuple (Tuple(..))
+
+import Screeps (Creep)
+import Screeps.Constants (find_sources)
+import Screeps.Controller as Controller
+import Screeps.Creep as Creep
+import Screeps.Game as Game
+import Screeps.Memory as Memory
+import Screeps.Room as Room
+import Screeps.RoomObject as RoomObject
+import Screeps.RoomPosition as RoomPosition
+
+import Types
+
+getCreepCapacity :: Creep -> Tuple Int Int
+getCreepCapacity creep = Tuple (Creep.totalAmtCarrying creep) (Creep.carryCapacity creep)
+
+generateReports :: Eff BaseScreepsEffects Reports
+generateReports = do
+  game <- Game.getGameGlobal
+  mem <- Memory.getMemoryGlobal
+  -- Hmm...
+  aiState <- (Memory.get mem "aiState") :: forall e. (EffScreepsCommand e) (Either String AiState)
+
+  let creeps = Game.creeps game
+  let maybeSpawn = M.lookup "Spawn1" $ Game.spawns game
+  let sources = maybe [] (\spawn -> Room.find (RoomObject.room spawn) find_sources) maybeSpawn
+
+  let creepCapacities = map getCreepCapacity creeps
+
+  pure $ Reports
+    { numberOfCreeps: maybe 0 id <<< Int.fromNumber $ M.size creeps
+    , creepCapacities: creepCapacities
+    , ticksToDowngrade: maybe 0 (\spawn -> case Room.controller $ RoomObject.room spawn of
+                                  Just controller -> Controller.ticksToDowngrade controller
+                                  Nothing -> 0) maybeSpawn
+    , sourceLocations: map (\source ->
+                        let roomPos = (RoomObject.pos source) in
+                            Point (RoomPosition.x roomPos) (RoomPosition.y roomPos)) sources
+    , creepLocations: map (\creep -> Point (RoomPosition.x (RoomObject.pos creep)) (RoomPosition.y (RoomObject.pos creep))) creeps
+    , creepInstructions: getCreepInstructions (either (const $ AiState { creepInstructions: M.empty, creepStates: M.empty }) id aiState)
+    , controllerLocation: maybe (Point 0 0) (\spawn -> case Room.controller $ RoomObject.room spawn of
+                                Just controller -> (Point (RoomPosition.x (RoomObject.pos controller)) (RoomPosition.y (RoomObject.pos controller)))
+                                Nothing -> (Point 0 0)) maybeSpawn
+    }
--- src/Main.purs
+++ src/Main.purs
@@ -7,26 +7,14 @@ import Control.Monad.Eff.Console (log)
 import Control.Monad.State.Trans (StateT(..), runStateT)
 
 import Data.Array (catMaybes, head, (:))
-import Data.Either (Either, either)
-import Data.Int as Int
-import Data.Maybe (Maybe(..), maybe)
 import Data.Newtype (unwrap)
 import Data.StrMap as M
-import Data.Tuple (Tuple(..), fst, snd)
+import Data.Tuple (Tuple, fst, snd)
 import Data.Traversable (traverse)
-
-import Screeps (Creep)
-import Screeps.Constants (find_sources)
-import Screeps.Controller as Controller
-import Screeps.Creep as Creep
-import Screeps.Game as Game
-import Screeps.Memory as Memory
-import Screeps.Room as Room
-import Screeps.RoomObject as RoomObject
-import Screeps.RoomPosition as RoomPosition
 
 import App.Execution as App.Execution
 import App.Memory as App.Memory
+import App.Reports as App.Reports
 
 import AI.Observations as AI.Observations
 import AI.StateManager as AI.StateManager
@@ -35,7 +23,7 @@ import Types
 
 main :: Eff BaseScreepsEffects Unit
 main = do
-  reports <- generateReports
+  reports <- App.Reports.generateReports
   let reportObservations = AI.Observations.analyzeReports reports
   log $ show reportObservations
 
@@ -51,35 +39,3 @@ main = do
   App.Memory.writeInstructionsToQueue $ fst instructionsAndNextState
 
   pure unit
-
-getCreepCapacity :: Creep -> Tuple Int Int
-getCreepCapacity creep = Tuple (Creep.totalAmtCarrying creep) (Creep.carryCapacity creep)
-
-generateReports :: Eff BaseScreepsEffects Reports
-generateReports = do
-  game <- Game.getGameGlobal
-  mem <- Memory.getMemoryGlobal
-  -- Hmm...
-  aiState <- (Memory.get mem "aiState") :: forall e. (EffScreepsCommand e) (Either String AiState)
-
-  let creeps = Game.creeps game
-  let maybeSpawn = M.lookup "Spawn1" $ Game.spawns game
-  let sources = maybe [] (\spawn -> Room.find (RoomObject.room spawn) find_sources) maybeSpawn
-
-  let creepCapacities = map getCreepCapacity creeps
-
-  pure $ Reports
-    { numberOfCreeps: maybe 0 id <<< Int.fromNumber $ M.size creeps
-    , creepCapacities: creepCapacities
-    , ticksToDowngrade: maybe 0 (\spawn -> case Room.controller $ RoomObject.room spawn of
-                                  Just controller -> Controller.ticksToDowngrade controller
-                                  Nothing -> 0) maybeSpawn
-    , sourceLocations: map (\source ->
-                        let roomPos = (RoomObject.pos source) in
-                            Point (RoomPosition.x roomPos) (RoomPosition.y roomPos)) sources
-    , creepLocations: map (\creep -> Point (RoomPosition.x (RoomObject.pos creep)) (RoomPosition.y (RoomObject.pos creep))) creeps
-    , creepInstructions: getCreepInstructions (either (const $ AiState { creepInstructions: M.empty, creepStates: M.empty }) id aiState)
-    , controllerLocation: maybe (Point 0 0) (\spawn -> case Room.controller $ RoomObject.room spawn of
-                                Just controller -> (Point (RoomPosition.x (RoomObject.pos controller)) (RoomPosition.y (RoomObject.pos controller)))
-                                Nothing -> (Point 0 0)) maybeSpawn
-    }
